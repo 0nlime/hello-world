3de672f38c69604d800dfd6bf67d34ff43e75a46
usuario_local <usuario_local@PTO0912>
2018-04-19 12:48:59 +0200

    hey!

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ class Hello {
 
     public static void main(String[] args) {
 
-        JOptionPane.showMessageDialog(null, "Lory Moneyyyyy, lololololo!!!");
+        JOptionPane.showMessageDialog(null, "Lory ");
 
     }
 
